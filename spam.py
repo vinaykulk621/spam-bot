3b76a8526484221e904447d8869a93eb31fb930b
vinay kulkarni <94120295+vinaykulk621@users.noreply.github.com>
2022-08-06 22:57:02 +0530

    Update spam.py

--- spam.py
+++ spam.py
@@ -1,21 +1,16 @@
 import pyautogui
 import time
 
-script=open(r'C:\Users\Kulka\OneDrive\Documents\GitHub\python\my_spam_bot\scriptt.txt')
-
+script=open(r'path/to/script.txt')
 read_it=script.read()
-
 write=read_it.split()
-
 print('YOU HAVE 5 SECONDS TO CLICK ON THE TYPE MESSEAGE IN WHATSAPP!!')
 
 count=5
-
 while(count!=0):
     print(count)
     time.sleep(1)
     count=count-1
-
 
 for i in range(0,len(write)):
     pyautogui.typewrite(write[i]+'\n')
